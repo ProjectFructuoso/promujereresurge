12e3fe9b385d3594101ebc7e851551fb57d3bd12
ProjectFructuoso <fabesoelangui@gmail.com>
2022-09-12 12:17:15 +0000

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
+
+// venobox
+
+$('.venobox').venobox();
+
+
+  AOS.init();
+
+
+
+
+ 
